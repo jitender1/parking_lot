91a91e61b3cc6f8262892672ddbd4c809f2552e6
jchauhan <jchauha@vmware.com>
2019-11-03 07:42:14 +0530

    adding Vehicle Pojo Object

---
+++ solution/src/main/java/com/gojek/parking/model/Vehicle.java
@@ -0,0 +1,34 @@
+package com.gojek.parking.model;
+
+/*
+ * Object Oriented representation of Vehicle
+ * This can be extended to any specific vehicle type like 4 Wheeler, 2 wheeler etc.
+ */
+public class Vehicle {
+    private String registrationNumber;
+    private String color;
+    private VehicleType vehicleType;
+
+    public Vehicle(String registrationNo, String color, VehicleType vehicleType){
+        this.registrationNumber = registrationNo;
+        this.color = color;
+        this.vehicleType = vehicleType;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public enum VehicleType {
+        TWO_WHEELER,
+        FOUR_WHEELER
+    }
+}
